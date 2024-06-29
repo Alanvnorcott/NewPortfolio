2bfbbbb77e66bcf84a564489480bafa28c4c70f6
Alanvnorcott <anorcott@drew.edu>
2024-06-29 03:29:17 -0400

    Need to slowww down

---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClasses,
+}: {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  otherClasses?: string;
+}) => {
+  return (
+    <button
+      className={`p-[3px] relative w-full rounded-lg md:w-60 md:mt-10 ${otherClasses}`}
+      onClick={handleClick}
+    >
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg transition-all duration-500 ease-in-out group-hover:from-purple-500 group-hover:to-indigo-500" />
+      <div className="px-8 py-2 bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent flex items-center justify-center space-x-2">
+        {position === 'left' && icon}
+        <span>{title}</span>
+        {position === 'right' && icon}
+      </div>
+    </button>
+  );
+};
+
+export default MagicButton;
