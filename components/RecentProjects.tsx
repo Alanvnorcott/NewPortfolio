4a3232b61401db4f247bc62b2310c056e2b74b7f
Alanvnorcott <anorcott@drew.edu>
2024-07-10 13:17:49 -0400

    okay

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
             A collection of {' '}
             <span className='text-blue-400'>recent projects</span>
         </h1>
-        <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-5 xs:gap-y-24 mt-10'>
+        <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-24 mt-10'>
             {projects.map(({ id, title, des, img, iconLists, link })=> (
                 <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]
                 flex items-center justify-center sm:w-[570px] w-[80vw]'>
